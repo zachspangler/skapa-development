57e5ee1d26f08fac07081e97a54e71755043e489
Zach Spangler <zaspangler@gmail.com>
2017-10-13 09:00:55 -0600

    Completed milestone 1 and submitted for review

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # skapa-development
 The purpose of this project is to create a personal 
-website that will  generate business for my freelance 
+website that will generate business for my freelance 
 development projects. Skapa Development seeks to 
 cost-efficiently solve small to medium business’ issues 
 through the creation of web applications.
--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<title>Personal Website Project | Milestone 1</title>
 		<meta charset="utf-8">
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Personal Website Project by Zach Spangler</h1>
 			<h2>Purpose, Audience, Goal</h2>
-				<h4>The Idea</h4>
+				<h4>The Website Idea</h4>
 					<p>For My personal website project (PWP) I will be creating a website for the freelance development
 						business I would like to start called, Skapa Development.</p>
 				<h4>Purpose:</h4>
@@ -21,11 +21,11 @@
 				<h4>Goal:</h4>
 					<p>My goal is to generate leads for freelance clients through the
 						exhibition of my development work. Additionally, I believe if I can demonstrate
-						the ability to create fully functional business, full-time employment opportunies will
+						the ability to create fully functional business, full-time employment opportunities will
 						be more likely.
 					</p>
 			<h2>Skapa Development Persona</h2>
-				<img src="./images/" alt="Skapa Development Persona">
+				<img src="../images/persona.jpg" alt="Skapa Development Persona">
 				<p><strong>Name:</strong> William K. Fredrick</p>
 				<p><strong>Age:</strong> 37</p>
 				<p><strong>Job:</strong> Owner/Operator of a small-to-Medium Business [10-100 employees]</p>
@@ -38,18 +38,17 @@
 					He wants to focus on growing his business instead of spending his time tackling this particular issue
 					on a regular basis. </p>
 			<p><strong>Technological Understanding:</strong> Medium to High</p>
-			<p><strong>Business Environment (use case):</strong> Must have stable internet connection to run the web
+			<p><strong>Business Environment:</strong> Must have stable internet connection to run the web
 				application that will be developed.</p>
 			<p><strong>Attitude:</strong> Frustrated and looking for a solution but have found other service
 				to be too expensive or non-existent for their particular use case.</p>
 			<p><strong>Business Type:</strong> Must not be a niche market for the business type the owner or operator has</p>
-			<p><strong>Relevant Questions from User:</strong>
+			<p><strong>Relevant Questions from User:</strong></p>
 				<ul>
 					<li>Pricing: How can I afford this solution?</li>
 					<li>Process of Development: How is this solution created?</li>
 					<li>History: What have you created before?</li>
 					<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
 				</ul>
-			</p>
 	</body>
 </html>
