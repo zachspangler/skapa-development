64bc815324f607cd66042f7ff24fd967bfca5db1
Zach Spangler <zaspangler@gmail.com>
2017-10-13 09:10:04 -0600

    Milestone 1 Complete  Done - Correctly set up your PWP using the git/GitHub/PhpStorm toolchain.  Done - Create the correct directory structure for the project exactly as outlined below.  Done - Define a purpose, audience, and goal for your project.  Done - Create one Persona for your project

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -8,10 +8,10 @@
 		<h1>Personal Website Project by Zach Spangler</h1>
 			<h2>Purpose, Audience, Goal</h2>
 				<h4>The Website Idea</h4>
-					<p>For My personal website project (PWP) I will be creating a website for the freelance development
+					<p>For my personal website project (PWP) I will be creating a website for the freelance development
 						business I would like to start called, Skapa Development.</p>
 				<h4>Purpose:</h4>
-					<p>To generate business for my freelance development
+					<p>The primary purpose of this website is to generate business for my freelance development
 						projects. My business will specialize in the creation of web applications
 						to efficiently solve business issues.</p>
 				<h4>Audience:</h4>
@@ -19,10 +19,10 @@
 						looking for ways to solve critical issues related
 						primarily to internal business issues. </p>
 				<h4>Goal:</h4>
-					<p>My goal is to generate leads for freelance clients through the
+					<p>The ultimate goal for this website is to generate leads for freelance clients through the
 						exhibition of my development work. Additionally, I believe if I can demonstrate
-						the ability to create fully functional business, full-time employment opportunities will
-						be more likely.
+						my ability to create fully functional websites that solve real business pain points for businesses,
+						full-time employment opportunities will be more likely and this will serve as a sort of portfolio.
 					</p>
 			<h2>Skapa Development Persona</h2>
 				<img src="../images/persona.jpg" alt="Skapa Development Persona">
@@ -39,11 +39,11 @@
 					on a regular basis. </p>
 			<p><strong>Technological Understanding:</strong> Medium to High</p>
 			<p><strong>Business Environment:</strong> Must have stable internet connection to run the web
-				application that will be developed.</p>
+				application that will be developed and be willing to use technology that allows him to access a web
+				browser (ideally a computer).</p>
 			<p><strong>Attitude:</strong> Frustrated and looking for a solution but have found other service
 				to be too expensive or non-existent for their particular use case.</p>
-			<p><strong>Business Type:</strong> Must not be a niche market for the business type the owner or operator has</p>
-			<p><strong>Relevant Questions from User:</strong></p>
+			<p><strong>Relevant Questions William will consider on my website:</strong></p>
 				<ul>
 					<li>Pricing: How can I afford this solution?</li>
 					<li>Process of Development: How is this solution created?</li>
