64b4a847026c955fed1dfa8383a9ae2442257c70
Zach Spangler <zaspangler@gmail.com>
2017-10-13 09:44:17 -0600

    Milestone 1 Complete  Done - Correctly set up your PWP using the git/GitHub/PhpStorm toolchain.  Done - Create the correct directory structure for the project exactly as outlined below.  Done - Define a purpose, audience, and goal for your project.  Done - Create one Persona for your project
     Review of grammar and spelling complete

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Personal Website Project by Zach Spangler</h1>
 			<h2>Purpose, Audience, Goal</h2>
-				<h4>The Website Idea</h4>
+				<h4>The Website Idea:</h4>
 					<p>For my personal website project (PWP) I will be creating a website for the freelance development
 						business I would like to start called, Skapa Development.</p>
 				<h4>Purpose:</h4>
@@ -15,40 +15,47 @@
 						projects. My business will specialize in the creation of web applications
 						to efficiently solve business issues.</p>
 				<h4>Audience:</h4>
-					<p>The audience for my website is small-medium business owners
-						looking for ways to solve critical issues related
-						primarily to internal business issues. </p>
+					<p>The audience for my website is business owners/operators of small to medium sized businesses (10-100
+						employees) who are looking for ways to solve critical issues related to internal business
+						processes.</p>
 				<h4>Goal:</h4>
 					<p>The ultimate goal for this website is to generate leads for freelance clients through the
-						exhibition of my development work. Additionally, I believe if I can demonstrate
-						my ability to create fully functional websites that solve real business pain points for businesses,
-						full-time employment opportunities will be more likely and this will serve as a sort of portfolio.
-					</p>
+						exhibition of my development work. I believe if I can demonstrate my ability to create fully
+						functional websites that solve real pain points for businesses, full-time employment
+						opportunities will be more likely. As a result, this will also serve as a professional portfolio.</p>
 			<h2>Skapa Development Persona</h2>
 				<img src="../images/persona.jpg" alt="Skapa Development Persona">
 				<p><strong>Name:</strong> William K. Fredrick</p>
 				<p><strong>Age:</strong> 37</p>
-				<p><strong>Job:</strong> Owner/Operator of a small-to-Medium Business [10-100 employees]</p>
-				<p><strong>Goal of Persona Internet Search:</strong> William is trying to find a solution for an
-					issue impeding the efficiency of his internal business process. William is not concerned with
-					taking a risk to solve a long-time business hurdle with the hope that this business efficiency
-					will improve. He has come to the point where the hurdle is causing to much inefficiency/disgruntled
-					employees or other business operations effects. William has a high understanding of how their industry
-					and internal processes work and a clear understanding of what his business needs to solve this problem.
-					He wants to focus on growing his business instead of spending his time tackling this particular issue
-					on a regular basis. </p>
-			<p><strong>Technological Understanding:</strong> Medium to High</p>
-			<p><strong>Business Environment:</strong> Must have stable internet connection to run the web
-				application that will be developed and be willing to use technology that allows him to access a web
-				browser (ideally a computer).</p>
-			<p><strong>Attitude:</strong> Frustrated and looking for a solution but have found other service
-				to be too expensive or non-existent for their particular use case.</p>
-			<p><strong>Relevant Questions William will consider on my website:</strong></p>
-				<ul>
-					<li>Pricing: How can I afford this solution?</li>
-					<li>Process of Development: How is this solution created?</li>
-					<li>History: What have you created before?</li>
-					<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
-				</ul>
+				<p><strong>Job:</strong> Owner/Operator of a Small-to-Medium Sized Business [10-100 employees]</p>
+				<p><strong>Goal of Persona's Internet Search:</strong> William is trying to find a solution for an
+					issue impeding the efficiency of his internal business processes. William is not concerned with
+					taking a risk to solve a long-time business pain point. He has come to the point where this problem is
+					causing inefficiency, disgruntled employees and having other negative effects on business operations
+					and overall environment.</p>
+				<p>William has a deep understanding of how his industry and internal processes work. He also has a
+					clear understanding of what his business needs to solve this problem but lacks the technical skills
+					to create the solution.</p>
+				<p>Ultimately, William wants to focus on growing his business instead of spending his time tackling
+					this particular issue on a regular basis.</p>
+				<p><strong>Technological Understanding:</strong> Medium to High</p>
+				<p><strong>Business Environment:</strong> There are two requirements for William's business enviroment:</p>
+					<ol>
+						<li>Must have stable internet connection to run the web application that will be developed</li>
+						<li>Must be willing to use technology that allows him to access a web browser</li>
+					</ol>
+				<p><strong>Attitude:</strong> Frustrated and looking for a solution. He has experienced one of the
+					following issues when looking for a solution:</p>
+					<ol>
+						<li>He has not been able to find a solution that addresses his particular pain point</li>
+						<li>If he did find a service, it is too expensive to justify the expense</li>
+					</ol>
+				<p><strong>Some Relevant Questions William will consider on my website:</strong></p>
+					<ul>
+						<li>Pricing: Can I afford this solution?</li>
+						<li>Process of Development: How is this solution created?</li>
+						<li>History: What has Skapa Development created before?</li>
+						<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
+					</ul>
 	</body>
 </html>
