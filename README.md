41779df70aee55af2295408a409d5c9cf55766b4
Zach Spangler <zaspangler@gmail.com>
2017-10-13 08:26:54 -0600

    Uploaded my Persona

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # skapa-development
-My personal website project for Deep Dive Bootcamp
+The purpose of this project is to create a personal 
+website that will  generate business for my freelance 
+development projects. Skapa Development seeks to 
+cost-efficiently solve small to medium business’ issues 
+through the creation of web applications.
---
+++ documentation/milestone-1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Personal Website Project | Milestone 1</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Personal Website Project by Zach Spangler</h1>
+			<h2>Purpose, Audience, Goal</h2>
+				<h4>The Idea</h4>
+					<p>For My personal website project (PWP) I will be creating a website for the freelance development
+						business I would like to start called, Skapa Development.</p>
+				<h4>Purpose:</h4>
+					<p>To generate business for my freelance development
+						projects. My business will specialize in the creation of web applications
+						to efficiently solve business issues.</p>
+				<h4>Audience:</h4>
+					<p>The audience for my website is small-medium business owners
+						looking for ways to solve critical issues related
+						primarily to internal business issues. </p>
+				<h4>Goal:</h4>
+					<p>My goal is to generate leads for freelance clients through the
+						exhibition of my development work. Additionally, I believe if I can demonstrate
+						the ability to create fully functional business, full-time employment opportunies will
+						be more likely.
+					</p>
+			<h2>Skapa Development Persona</h2>
+				<img src="./images/" alt="Skapa Development Persona">
+				<p><strong>Name:</strong> William K. Fredrick</p>
+				<p><strong>Age:</strong> 37</p>
+				<p><strong>Job:</strong> Owner/Operator of a small-to-Medium Business [10-100 employees]</p>
+				<p><strong>Goal of Persona Internet Search:</strong> William is trying to find a solution for an
+					issue impeding the efficiency of his internal business process. William is not concerned with
+					taking a risk to solve a long-time business hurdle with the hope that this business efficiency
+					will improve. He has come to the point where the hurdle is causing to much inefficiency/disgruntled
+					employees or other business operations effects. William has a high understanding of how their industry
+					and internal processes work and a clear understanding of what his business needs to solve this problem.
+					He wants to focus on growing his business instead of spending his time tackling this particular issue
+					on a regular basis. </p>
+			<p><strong>Technological Understanding:</strong> Medium to High</p>
+			<p><strong>Business Environment (use case):</strong> Must have stable internet connection to run the web
+				application that will be developed.</p>
+			<p><strong>Attitude:</strong> Frustrated and looking for a solution but have found other service
+				to be too expensive or non-existent for their particular use case.</p>
+			<p><strong>Business Type:</strong> Must not be a niche market for the business type the owner or operator has</p>
+			<p><strong>Relevant Questions from User:</strong>
+				<ul>
+					<li>Pricing: How can I afford this solution?</li>
+					<li>Process of Development: How is this solution created?</li>
+					<li>History: What have you created before?</li>
+					<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
+				</ul>
+			</p>
+	</body>
+</html>
