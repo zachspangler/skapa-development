cbf002b580987f8858c88ffc49fe784f006c35c7
Zach Spangler <zaspangler@gmail.com>
2017-10-17 11:36:06 -0600

    Created PWP Milestone 2 and updated wireframes

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -5,20 +5,23 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>Content Strategy</h2>
-			<p>My content strategy for Skapa Development is to promote previous work and highlight the business concept
-				by highlighting the lower cost of development, custom solutions and the 7+ years of business knowledge we
-				with us.</p>
-			<p>Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as
-				“to bring into existence.” At Skapa our goal is to created tools to eliminate headaches for small business
-				owners through web development.</p>
-			<p>We aim to cost effectively develop these tools for you through a unique partnership between you and Skapa.
-				Our goal is to bring these tools to life without you having to spend tens of thousands of dollars on a
-				custom solution.</p>
-		<h2>Wireframe</h2>
-			<h5>Mobile Wireframe</h5>
-				<img src="/images/pwp-mobile-wireframe.jpg" alt="Mobile Wireframe - Skapa Development">
-			<h5>Desktop Wireframe</h5>
-				<img src="/images/pwp-desktop-wireframe.jpg" alt="Desktop Wireframe - Skapa Development">
+		<h1>Milestone 2</h1>
+			<h2>Content Strategy</h2>
+				<p>My content strategy for Skapa Development is to promote previous work and highlight the business concept
+					by highlighting the lower cost of development, custom solutions and the 7+ years of business knowledge we
+					with us.</p>
+				<p>Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as
+					“to bring into existence.” At Skapa our goal is to create tools to eliminate headaches for business
+					owners through the development of web applications.</p>
+				<p>We aim to cost effectively develop these tools for you through a unique partnership between you and Skapa.
+					Our goal is to bring these tools to life without you having to spend tens of thousands of dollars on a
+					custom solution.</p>
+		<h2>Wireframes</h2>
+				<h3>Desktop Wireframe</h3>
+					<img src="../images/pwp-desktop-wireframe.jpg" alt="Desktop Wireframe - Skapa Development"
+						  height="50%" width="50%">
+				<h3>Mobile Wireframe</h3>
+					<img src="../images/pwp-mobile-wireframe.jpg" alt="Mobile Wireframe - Skapa Development" height="50%" width="50%">>
+
 	</body>
 </html>
