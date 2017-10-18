f6c8e19529447ce3e863aebe075ed8d6e33a8a9a
Zach Spangler <zaspangler@gmail.com>
2017-10-18 10:32:31 -0600

    Updated HTML and moved images into documentation

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -6,56 +6,53 @@
 	</head>
 	<body>
 		<h1>Personal Website Project by Zach Spangler</h1>
-			<h2>Purpose, Audience, Goal</h2>
-				<h4>The Website Idea:</h4>
-					<p>For my personal website project (PWP) I will be creating a website for the freelance development
-						business I would like to start called, Skapa Development.</p>
-				<h4>Purpose:</h4>
-					<p>The primary purpose of this website is to generate business for my freelance development
-						projects. My business will specialize in the creation of web applications
-						to efficiently solve business issues.</p>
-				<h4>Audience:</h4>
-					<p>The audience for my website is business owners/operators of small to medium sized businesses (10-100
-						employees) who are looking for ways to solve critical issues related to internal business
-						processes.</p>
-				<h4>Goal:</h4>
-					<p>The ultimate goal for this website is to generate leads for freelance clients through the
-						exhibition of my development work. I believe if I can demonstrate my ability to create fully
-						functional websites that solve real pain points for businesses, full-time employment
-						opportunities will be more likely. As a result, this will also serve as a professional portfolio.</p>
-			<h2>Skapa Development Persona</h2>
-				<img src="../images/persona.jpg" alt="Skapa Development Persona">
-				<p><strong>Name:</strong> William K. Fredrick</p>
-				<p><strong>Age:</strong> 37</p>
-				<p><strong>Job:</strong> Owner/Operator of a Small-to-Medium Sized Business [10-100 employees]</p>
-				<p><strong>Goal of Persona's Internet Search:</strong> William is trying to find a solution for an
-					issue impeding the efficiency of his internal business processes. William is not concerned with
-					taking a risk to solve a long-time business pain point. He has come to the point where this problem is
-					causing inefficiency, disgruntled employees and having other negative effects on business operations
-					and overall environment.</p>
-				<p>William has a deep understanding of how his industry and internal processes work. He also has a
-					clear understanding of what his business needs to solve this problem but lacks the technical skills
-					to create the solution.</p>
-				<p>Ultimately, William wants to focus on growing his business instead of spending his time tackling
-					this particular issue on a regular basis.</p>
-				<p><strong>Technological Understanding:</strong> Medium to High</p>
-				<p><strong>Business Environment:</strong> There are two requirements for William's business enviroment:</p>
-					<ol>
-						<li>Must have stable internet connection to run the web application that will be developed</li>
-						<li>Must be willing to use technology that allows him to access a web browser</li>
-					</ol>
-				<p><strong>Attitude:</strong> Frustrated and looking for a solution. He has experienced one of the
-					following issues when looking for a solution:</p>
-					<ol>
-						<li>He has not been able to find a solution that addresses his particular pain point</li>
-						<li>If he did find a service, it is too expensive to justify the expense</li>
-					</ol>
-				<p><strong>Some Relevant Questions William will consider on my website:</strong></p>
-					<ul>
-						<li>Pricing: Can I afford this solution?</li>
-						<li>Process of Development: How is this solution created?</li>
-						<li>History: What has Skapa Development created before?</li>
-						<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
-					</ul>
+		<h2>Purpose, Audience, Goal</h2>
+		<h4>The Website Idea:</h4>
+		<p>For my personal website project (PWP) I will be creating a website for the freelance development business I
+			would like to start called, Skapa Development.</p>
+		<h4>Purpose:</h4>
+		<p>The primary purpose of this website is to generate business for my freelance development projects. My
+			business will specialize in the creation of web applications to efficiently solve business issues.</p>
+		<h4>Audience:</h4>
+		<p>The audience for my website is business owners/operators of small to medium sized businesses (10-100
+			employees) who are looking for ways to solve critical issues related to internal business processes.</p>
+		<h4>Goal:</h4>
+		<p>The ultimate goal for this website is to generate leads for freelance clients through the exhibition of my
+			development work. I believe if I can demonstrate my ability to create fully functional websites that solve
+			real pain points for businesses, full-time employment opportunities will be more likely. As a result, this
+			will also serve as a professional portfolio.</p>
+		<h2>Skapa Development Persona</h2>
+		<img src="./documentatonimages/persona.jpg" alt="Skapa Development Persona">
+		<p><strong>Name:</strong> William K. Fredrick</p>
+		<p><strong>Age:</strong> 37</p>
+		<p><strong>Job:</strong> Owner/Operator of a Small-to-Medium Sized Business [10-100 employees]</p>
+		<p><strong>Goal of Persona's Internet Search:</strong> William is trying to find a solution for an issue
+			impeding the efficiency of his internal business processes. William is not concerned with taking a risk to
+			solve a long-time business pain point. He has come to the point where this problem is causing inefficiency,
+			disgruntled employees and having other negative effects on business operations and overall environment.</p>
+		<p>William has a deep understanding of how his industry and internal processes work. He also has a clear
+			understanding of what his business needs to solve this problem but lacks the technical skills to create the
+			solution.</p>
+		<p>Ultimately, William wants to focus on growing his business instead of spending his time tackling this
+			particular issue on a regular basis.</p>
+		<p><strong>Technological Understanding:</strong> Medium to High</p>
+		<p><strong>Business Environment:</strong> There are two requirements for William's business enviroment:</p>
+		<ol>
+			<li>Must have stable internet connection to run the web application that will be developed</li>
+			<li>Must be willing to use technology that allows him to access a web browser</li>
+		</ol>
+		<p><strong>Attitude:</strong> Frustrated and looking for a solution. He has experienced one of the
+			following issues when looking for a solution:</p>
+		<ol>
+			<li>He has not been able to find a solution that addresses his particular pain point</li>
+			<li>If he did find a service, it is too expensive to justify the expense</li>
+		</ol>
+		<p><strong>Some Relevant Questions William will consider on my website:</strong></p>
+		<ul>
+			<li>Pricing: Can I afford this solution?</li>
+			<li>Process of Development: How is this solution created?</li>
+			<li>History: What has Skapa Development created before?</li>
+			<li>Trust: Who would I be working with? How can I trust you? What is your experience?</li>
+		</ul>
 	</body>
 </html>
