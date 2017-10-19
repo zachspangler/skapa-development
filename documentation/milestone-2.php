7dcf0684982d87795fefd8dabdf391f1bba8ea96
Zach Spangler <zaspangler@gmail.com>
2017-10-19 16:39:57 -0600

    updated milestone 2 content strategy plan

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -10,7 +10,7 @@
 		<p><strong>How It Works:</strong> This section will content strategy for Skapa Development is to promote previous work and highlight the business concept by detailing the partnership structure which results in a lower cost of development, custom solution and the 7+ years of both enterprise and startup business knowledge.</p>
 		<p><strong>Chart Data:</strong> These charts will focus on potential cost savings of and benefits of using Skapa Development.</p>
 		<p><strong>Projects:</strong> The projects section will feature three prominent development projects: The projects to be featured are TBD.</p>
-		<p><strong>About Us:</strong> This section will focus on our company, I am thinking something like this: "Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as “to bring into existence.” At Skapa our goal is to create tools to eliminate headaches for business owners through the development of web applications."</p>
+		<p><strong>About Us:</strong> This section will focus on our company, I am thinking something like this: Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as “to bring into existence.” At Skapa our goal is to create tools to eliminate headaches for business owners through the development of web applications.</p>
 		<p><strong>Contact Us:</strong> This section will contain our contact form, the Skapa Development email (info@skapadevelopment.com). Additional links to my professional social media profiles will be included in the footer: LinkedIn, GitHub, Facebook.</p>
 		<h2>Wireframes</h2>
 		<h3>Desktop Wireframe</h3>
