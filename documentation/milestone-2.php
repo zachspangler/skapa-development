f2499d671900ffaf9f7cec0ce7d8db11e1a18610
Zach Spangler <zaspangler@gmail.com>
2017-10-19 16:34:12 -0600

    updated milestone 2 content strategy plan

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -7,9 +7,11 @@
 	<body>
 		<h1>Milestone 2</h1>
 		<h2>Content Strategy</h2>
-		<p>My content strategy for Skapa Development is to promote previous work and highlight the business concept by highlighting the lower cost of development, custom solutions and the 7+ years of business knowledge we with us.</p>
-		<p>Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as “to bring into existence.” At Skapa our goal is to create tools to eliminate headaches for business owners through the development of web applications.</p>
-		<p>We aim to cost effectively develop these tools for you through a unique partnership between you and Skapa. Our goal is to bring these tools to life without you having to spend tens of thousands of dollars on a custom solution.</p>
+		<p><strong>How It Works:</strong> This section will content strategy for Skapa Development is to promote previous work and highlight the business concept by detailing the partnership structure which results in a lower cost of development, custom solution and the 7+ years of both enterprise and startup business knowledge.</p>
+		<p><strong>Chart Data:</strong> These charts will focus on potential cost savings of and benefits of using Skapa Development.</p>
+		<p><strong>Chart Data:</strong> These charts will focus on potential cost savings of and benefits of using Skapa Development.</p>
+		<p><strong>Projects:</strong> The projects will feature eventually feature three prominent development projects: The projects to be featured are TBD.</p>
+		<p><strong>Contact Us</strong>Contact: This section will contain our contact form, the Skapa Development email (info@skapadevelopment.com). Additional links to my professional social media profiles will be included in the footer: LinkedIn, GitHub, Facebook.</p>
 		<h2>Wireframes</h2>
 		<h3>Desktop Wireframe</h3>
 		<img src="./documentatonimages/pwp-desktop-wireframe.jpg" alt="Desktop Wireframe - Skapa Development" height="50%" width="50%">
