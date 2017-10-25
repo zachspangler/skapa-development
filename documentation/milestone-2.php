8b04a9cc031899b9dc5fdf5f45028a0834f67442
Zach Spangler <zaspangler@gmail.com>
2017-10-25 14:39:52 -0600

    updated milestone 2 content strategy plan

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -7,8 +7,8 @@
 	<body>
 		<h1>Milestone 2</h1>
 		<h2>Content Strategy</h2>
-		<p><strong>How It Works:</strong> This section will content strategy for Skapa Development is to promote previous work and highlight the business concept by detailing the partnership structure which results in a lower cost of development, custom solution and the 7+ years of both enterprise and startup business knowledge.</p>
-		<p><strong>Chart Data:</strong> These charts will focus on potential cost savings of and benefits of using Skapa Development.</p>
+		<p><strong>How It Works:</strong> The content strategy for this section is to provide a high level understanding of Skapa Development's unique business concept which focus on  a partnership structure which results in a lower cost of development, a custom development solution and the 7+ years of both enterprise and startup business knowledge I bring.</p>
+		<p><strong>Chart Data:</strong> These charts will focus on potential cost savings of and benefits of using Skapa Development based on other projects outcomes and online research.</p>
 		<p><strong>Projects:</strong> The projects section will feature three prominent development projects: The projects to be featured are TBD.</p>
 		<p><strong>About Us:</strong> This section will focus on our company, I am thinking something like this: Our name comes from the Swedish verb meaning: to create. Merriam-Webster dictionary defines create as “to bring into existence.” At Skapa our goal is to create tools to eliminate headaches for business owners through the development of web applications.</p>
 		<p><strong>Contact Us:</strong> This section will contain our contact form, the Skapa Development email (info@skapadevelopment.com). Additional links to my professional social media profiles will be included in the footer: LinkedIn, GitHub, Facebook.</p>
