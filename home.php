854af6bc898dc2b42f513a302a02ed7ef0f74b3f
Zach Spangler <zaspangler@gmail.com>
2017-11-01 16:36:28 -0600

    added some placeholders

--- home.php
+++ home.php
@@ -8,7 +8,8 @@
 		<!----Google Fonts---->
 		<link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet">
 		<!----Bootstrap Link---->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+				integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<!----Skapa Development CSS---->
 		<link rel="stylesheet" type="text/css" href="styles.css">
 	</head>
@@ -16,8 +17,8 @@
 		<!----Nav Bar---->
 		<nav></nav>
 		<!----Banner---->
-		<header class="masthead">
-			<div class="header-content">
+		<header class="banner">
+			<div class="header-banner">
 				<div class="header-content-inner">
 					<h1 id="company">SKAPA DEVELOPMENT</h1>
 				</div>
@@ -32,7 +33,9 @@
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
 					<div class="col">
-						1 of 2
+						<div class="card">
+							<img class="card-img-top" src="images/gear.jpg" alt="Lalana">
+						</div>
 					</div>
 				</div>
 			</div>
@@ -44,17 +47,17 @@
 				<div class="row">
 					<div class="col">
 						<div class="card">
-							<img class="card-img-top" src="..." alt="Lalana">
+							<img class="card-img-top" src="images/pattern.jpg" alt="Lalana">
 						</div>
 					</div>
 					<div class="col">
 						<div class="card">
-							<img class="card-img-top" src="..." alt="CrowdVibe">
+							<img class="card-img-top" src="images/pattern.jpg" alt="CrowdVibe">
 						</div>
 					</div>
 					<div class="col">
 						<div class="card">
-							<img class="card-img-top" src="..." alt="Handoff">
+							<img class="card-img-top" src="images/pattern.jpg" alt="Handoff">
 						</div>
 					</div>
 				</div>
@@ -64,7 +67,9 @@
 			<div class="container">
 				<div class="row">
 					<div class="col">
-						1 of 2
+						<div class="card">
+							<img class="card-img-top" src="images/about_us.jpg" alt="Lalana">
+						</div>
 					</div>
 					<div class="col">
 						<h4>About Us</h4>
@@ -92,9 +97,15 @@
 			<p>&copy2017 Skapa Development. All Right Reserved</p>
 		</footer>
 		<!-- jQuery first, then Tether, then Bootstrap JS. -->
-		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+				  integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+				  integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+				  integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+				  crossorigin="anonymous"></script>
 	</body>
 	</body>
 </html>
