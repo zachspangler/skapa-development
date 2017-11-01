148d0a0ce0497c3d88369374d382012a4192ac5b
Zach Spangler <zaspangler@gmail.com>
2017-11-01 17:13:48 -0600

    added additional placeholders for charts

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
 		<header class="banner">
 			<div class="header-banner">
 				<div class="header-content-inner">
-					<h1 id="company">SKAPA DEVELOPMENT</h1>
+					<p id="company">SKAPA DEVELOPMENT</p>
 				</div>
 			</div>
 		</header>
@@ -33,8 +33,21 @@
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
 					<div class="col">
-						<div class="card">
-							<img class="card-img-top" src="images/gear.jpg" alt="Lalana">
+						<div class="row">
+							<div class="col-8 col-sm-6">
+								<img src="images/chart.png" alt="chart">
+							</div>
+							<div class="col-4 col-sm-6">
+								<img src="images/chart.png" alt="chart">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-8 col-sm-6">
+								<img src="images/chart.png" alt="chart">
+							</div>
+							<div class="col-4 col-sm-6">
+								<img src="images/chart.png" alt="chart">
+							</div>
 						</div>
 					</div>
 				</div>
