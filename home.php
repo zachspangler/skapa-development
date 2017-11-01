bb65be45b4d57a1478a8a94f0e0b9d028c232c47
Zach Spangler <zaspangler@gmail.com>
2017-11-01 15:00:02 -0600

    setup html shell for my Skapa Development

--- home.php
+++ home.php
@@ -72,7 +72,9 @@
 			<h2>Contact Us</h2>
 		</section>
 		<!----Footer---->
-		<footer></footer>
+		<footer>
+			&copy 2017 Skapa Development. All Right Reserved
+		</footer>
 		<!-- jQuery first, then Tether, then Bootstrap JS. -->
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
