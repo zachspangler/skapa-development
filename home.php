e882bd89d2e139003787e030442334b7b25838cd
Zach Spangler <zaspangler@gmail.com>
2017-11-01 14:56:42 -0600

    setup html shell for my Skapa Development

---
+++ home.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Skapa Development</title>
+		<!----Font Awesome Link---->
+		<script src="https://use.fontawesome.com/46c6e23a21.js"></script>
+		<!----Google Fonts---->
+		<link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet">
+		<!----Bootstrap Link---->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<!----Skapa Development CSS---->
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+	<body>
+		<!----Nav Bar---->
+		<nav></nav>
+		<!----Banner---->
+		<header class="masthead">
+			<div class="header-content">
+				<div class="header-content-inner">
+					<h1 id="company">SKAPA DEVELOPMENT</h1>
+				</div>
+			</div>
+		</header>
+		<!----How It Works---->
+		<section id="about">
+			<h2>How It Works</h2>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						1 of 2
+					</div>
+					<div class="col">
+						1 of 2
+					</div>
+				</div>
+			</div>
+		</section>
+		<!----Projects---->
+		<section id="projects">
+			<h2>Projects</h2>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						1 of 3
+					</div>
+					<div class="col">
+						1 of 3
+					</div>
+					<div class="col">
+						1 of 3
+					</div>
+				</div>
+		</section>
+		<!----About Us---->
+		<section id="about-us">
+			<h2>About Us</h2>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						1 of 2
+					</div>
+					<div class="col">
+						1 of 2
+					</div>
+				</div>
+			</div>
+		</section>
+		<!----Contact Us---->
+		<section id="contact-us">
+			<h2>Contact Us</h2>
+		</section>
+		<!----Footer---->
+		<footer></footer>
+		<!-- jQuery first, then Tether, then Bootstrap JS. -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</body>
+	</body>
+</html>
