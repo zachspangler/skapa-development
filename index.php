d6ae2ebfb375a2ca087d7c77a2900c448eb35d69
Zach Spangler <zaspangler@gmail.com>
2017-11-20 15:07:11 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!----Required Meta Tags---->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Skapa Development</title>
+		<!----Font Awesome Link---->
+		<script src="https://use.fontawesome.com/46c6e23a21.js"></script>
+		<!----Google Fonts---->
+		<link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Vollkorn+SC" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+		<!----Bootstrap Link---->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+				integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<!----Skapa Development CSS---->
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<!----Skapa Favicon---->
+		<link rel="shortcut icon" href="images/lamp-favicon.jpg" type="image/x-icon">
+	</head>
+	<body>
+		<!----Banner---->
+		<div class="jumbotron">
+			<!----Nav Bar---->
+			<nav class="navbar navbar-expand-lg">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+						  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#what-we-do">What We Do</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#projects">Featured Work</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#the-process">The Process</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#contact-us">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<p class="title">SKAPA</p>
+			<p class="title2">DEVELOPMENT</p>
+			<p class="subtext">Creating Software To Solve Your Problems</p>
+		</div>
+		<!----How It Works---->
+		<section id="what-we-do">
+			<p id="section-heading">WHAT WE DO</p>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<p id="about-text">We cover a large range of creative digital projects, platforms, and campaigns with one purpose: <span id="yellow-text">to create experiences</span>.</p>
+						<p id="about-subtext">--We transform brands, grow businesses, and tell brand and product stories in a most creative and fascinating way.</p>
+					</div>
+					<div class="col-md-6"></div>
+				</div>
+		</section>
+		<!----Projects---->
+		<section id="projects">
+			<p id="section-heading-white">FEATURED WORK</p>
+			<div class="container">
+				<div class="row lalana">
+					<div class="col-sm-6">
+						<h3 id="project-name">Lalana</h3>
+						<ul>
+							<li><span id="project-details">Issue:</span> Lorem ipsum dolor sit amet</li>
+							<li><span id="project-details">Solution:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</li>
+							<li><span id="project-details">Impact:</span> Lorem ipsum dolor sit amet</li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<a href="http://www.espn.com/" target="_blank">
+							<img src="images/lalana.png " alt="Lalana Tracking" class="mx-auto d-block">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row crowdvibe">
+					<div class="col-sm-6">
+						<a href="http://www.espn.com/" target="_blank">
+							<img src="images/crowdvibev2.png" alt="Crowdvibe" class="mx-auto d-block"></a>
+					</div>
+					<div class="col-sm-6">
+						<h3 id="project-name">CrowdVibe</h3>
+						<ul>
+							<li><span id="project-details">Issue:</span> Lorem ipsum dolor sit amet</li>
+							<li><span id="project-details">Solution:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</li>
+							<li><span id="project-details">Impact:</span> Lorem ipsum dolor sit amet</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row handoff">
+					<div class="col-sm-6">
+						<h3 id="project-name">Handoff</h3>
+						<ul>
+							<li><span id="project-details">Issue:</span> Lorem ipsum dolor sit amet</li>
+							<li><span id="project-details">Solution:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</li>
+							<li><span id="project-details">Impact:</span> Lorem ipsum dolor sit amet</li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<a href="http://www.espn.com/" target="_blank">
+							<img src="images/handoff.png" alt="Handoff" class="mx-auto d-block "></a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!----About Us---->
+		<section id="the-process" class="img-fluid">
+			<p id="section-heading">THE PROCESS</p>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<p class="container-fluid" id="process-number-odd">01</p>
+					</div>
+					<div class="col-md-7">
+						<p id="process-name">RESEARCH & IDEATION</p>
+						<p id="process-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-3">
+						<p class="container-fluid" id="process-number-two">02</p>
+					</div>
+					<div class="col-md-6">
+						<p id="process-name">PROTOTYPING</p>
+						<p id="process-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<p class="container-fluid" id="process-number-odd">03</p>
+					</div>
+					<div class="col-md-7">
+						<p id="process-name">ITERATION</p>
+						<p id="process-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-4">
+						<p class="container-fluid" id="process-number-four">04</p>
+					</div>
+					<div class="col-md-6">
+						<p id="process-name">FINAL EXPERIENCE</p>
+						<p id="process-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+					<div class="col-md-3"></div>
+				</div>
+			</div>
+		</section>
+		<!----Contact Us---->
+		<section id="contact-us">
+			<p id="contact-us-text">Interested in working together?</p>
+			<p id="contact-us-subtext">We are always looking for new partners and projects</p>
+			<p id="contact-us-subtext">Shoot us an email or fill out the contact form below to get the conversation started</p>
+			<!--			<div class="row">-->
+			<!--				<div class="col"></div>-->
+			<!--				<div class="col">-->
+			<!--					<form class="form-horizontal">-->
+			<!--						<div class="form-group">-->
+			<!--							<div class="input-group">-->
+			<!--								<div class="input-group-addon">-->
+			<!--									<i class="fa fa-user"></i>-->
+			<!--								</div>-->
+			<!--								<input type="text" id="customername" placeholder="Name"/>-->
+			<!--							</div>-->
+			<!--						</div>-->
+			<!--						<div class="form-group">-->
+			<!--							<div class="input-group">-->
+			<!--								<div class="input-group-addon">-->
+			<!--									<i class="fa fa-envelope"></i>-->
+			<!--								</div>-->
+			<!--								<input type="email" id="email" placeholder="Email">-->
+			<!--							</div>-->
+			<!--						</div>-->
+			<!--						<div class="form-group">-->
+			<!--							<div class="input-group">-->
+			<!--								<div class="input-group-addon">-->
+			<!--									<i class="fa fa-phone"></i>-->
+			<!--								</div>-->
+			<!--								<input type="tel" id="phone" placeholder="Phone">-->
+			<!--							</div>-->
+			<!--						</div>-->
+			<!--						<div class="form-group">-->
+			<!--							<div class="input-group">-->
+			<!--								<div class="input-group-addon">-->
+			<!--									<i class="fa fa-female"></i>-->
+			<!--								</div>-->
+			<!--								<input type="subject" id="subject" placeholder="Subject">-->
+			<!--							</div>-->
+			<!--						</div>-->
+			<!--						<div class="form-group">-->
+			<!--							<div class="input-group">-->
+			<!--								<div class="input-group-addon">-->
+			<!--									<i class="fa fa-comment"></i>-->
+			<!--								</div>-->
+			<!--								<textarea type="text" id="message" placeholder="Message"></textarea>-->
+			<!--							</div>-->
+			<!--						</div>-->
+			<!--						<input type="submit" value="Submit">-->
+			<!--					</form>-->
+			<button type="button">Contact</button>
+			</div>
+			<div class="col"></div>
+		</section>
+		<!----Footer---->
+		<footer>
+			<div class="row">
+				<div class="col-md-6">
+					<p id="our-office">Our Office</p>
+					<p id="location">Albuquerque</p>
+					<p>hello@skapadevelopment.com</p>
+					<p>505-555-5555</p>
+					<p>525 Buena Vista Dr SE</p>
+					<p>Albuquerque, NM 87106</p>
+				</div>
+				<div class="col-md-6">
+					<p id="reach-out">Reach Out</p>
+					<a href="#" target="_blank" id="reach-out"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+					<a href="#" target="_blank" id="reach-out"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+					<a href="#" target="_blank" id="reach-out"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+					<a href="#" target="_blank" id="reach-out"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+					<p id="reach=out">&copy2017 Skapa Development</p>
+				</div>
+				<div class="col-2"></div>
+			</div>
+		</footer>
+		<!-- jQuery first, then Tether, then Bootstrap JS. -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+				  integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+				  integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+				  integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+				  crossorigin="anonymous"></script>
+	</body>
+</html>
